96d1f87eca1983d0946a77c487fc97463b5050a6
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-15 20:21:56 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]'),
+    btnStop = document.querySelector('[data-stop]');
+let interval = null;  
+
+const onBtnStartClick = (e) => {
+    interval = setInterval(chengeColor, 1000);
+    e.target.disabled = true;
+    btnStop.disabled = false;
+};
+
+const onBtnStopClick = (e) => {
+    clearInterval(interval);
+    btnStart.disabled = false;
+    e.target.disabled = true;
+};
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function chengeColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
