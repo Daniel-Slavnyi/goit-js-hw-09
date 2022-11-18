8cf4d1c265ce984f8da66f17ff8f95a18eda5cb1
Daniel-Slavnyi <113385648+Daniel-Slavnyi@users.noreply.github.com>
2022-11-18 15:16:29 +0000

    Deploying to gh-pages from @ Daniel-Slavnyi/goit-js-hw-09@9acd01581dc15077986d4f813de2dd66c519de26 🚀

--- 03-promises.622255ba.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),n={};e.addEventListener("input",(function(e){var t=e.target,r=t.name,a=t.value;n[r]=a}))}();
-//# sourceMappingURL=03-promises.622255ba.js.map
--- 03-promises.a3ebeffc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),t={};e.addEventListener("input",(e=>{const{name:n,value:o}=e.target;t[n]=o}));
-//# sourceMappingURL=03-promises.a3ebeffc.js.map
